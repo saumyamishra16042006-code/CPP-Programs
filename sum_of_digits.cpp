7197b63f1367b3ea1e675f1611e13c46b0d526b3
Saumya-codes <saumya.mishra16042006@gmail.com>
2026-07-12 17:56:06 +0530

    add sum_of_digits.cpp program

---
+++ sum_of_digits.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int n;
+   cout<<"enter n:";
+   cin>>n;
+  int sum=0;
+  for(int i=1;n!=0;i++){
+     int ld=n%10;
+     n=n/10;
+    sum+=ld;
+  }
+  cout<<sum<<endl;
+   return 0;
+}
